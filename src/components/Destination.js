a70e7a812d7de578b8d812cd8f92471d0cd57ae0
3vilBonobo <iboutsika@gmail.com>
2022-02-09 23:17:59 +0200

    css additions

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { FaMapMarkerAlt } from 'react-icons/fa';
 
 const Destination = (props) => {
   return (
     <div className="destination-card">
       <img className="image" src={props.item.imageUrl} alt={props.item.title} />
       <div className="info-section">
-        <div className="gmaps">
+        <div className="g-maps">
+          <i>
+            <FaMapMarkerAlt color="#f55a5a" />
+          </i>
           <h2 className="country">{props.item.location}</h2>
           <a className="map-link" src={props.item.googleMapsUrl}>
             View on Google Maps
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { GiEarthAmerica } from 'react-icons/gi';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
+      <GiEarthAmerica fontSize="28px" />
       <h1 className="nav-title">my travel journal.</h1>
     </nav>
   );
